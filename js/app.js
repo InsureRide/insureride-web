d0a825ffdf597cc15f0fcd3aa9fa6c1fec1a3ef9
Moritz Schneider <scmo@zhaw.ch>
2017-03-18 21:04:32 +0100

    new logo, favicons

--- js/app.js
+++ js/app.js
@@ -1,18 +1,19 @@
+var API = 'https://insureride.net/api/v1';
+// var API = 'http://localhost:8080/v1';
 
 
 $( document ).ready(function() {
-   
     var car = {};
 
     function loadDrives(drives) {
 
-        for(var i = 0; i<drives.length(); i++){
-
-
-        var drive = '<div class="col-md-3"> <div class="panel panel-default drive-panel"> <div class="drive-row"> <div class="route-from-section"> <div class="ride-infotitle"> From </div> <div class="ride-info-content"> St. Gallen </div> </div> <div class="route-to-section"> <div class="ride-infotitle"> To </div> <div class="ride-info-content"> Munich </div> </div> <br style="clear: left;" /> </div> <div class="route-locations-section"> <div class="drive-row"> <div class="ride-infotitle"> Date </div> <div class="ride-info-content" > 17.03.2017 </div> </div> <div class="drive-row"> <div class="ride-infotitle"> Driving Score </div> <div class="ride-info-content" style="color: #27ae60"> Very Safe Drive </div> </div> <!-- <div class="drive-row"> <div class="route-from-section"> <div class="ride-infotitle"> Distance </div> <div class="ride-info-content"> 233 km </div> </div> <div class="route-to-section"> <div class="ride-infotitle"> Time </div> <div class="ride-info-content"> 2h 33min </div> </div> <br style="clear: left;" /> </div> --> <div class="drive-row"> <div class="ride-infotitle"> Insurance Cost </div> <div class="ride-info-content"> CHF 9.15 </div> </div> <div class="drive-moreinfo-wrapper"> <button type="button" class="btn btn-primary btn-md drive-details-btn"> Details </button> </div> </div> </div> </div>'
+        for(var i = 0; i< drives.length; i++){
+            console.log('asdf');
+            var drive = '<div class="col-md-3"> <div class="panel panel-default drive-panel"> <div class="drive-row"> <div class="route-from-section"> <div class="ride-infotitle"> From </div> <div class="ride-info-content"> St. Gallen </div> </div> <div class="route-to-section"> <div class="ride-infotitle"> To </div> <div class="ride-info-content"> Munich </div> </div> <br style="clear: left;" /> </div> <div class="route-locations-section"> <div class="drive-row"> <div class="ride-infotitle"> Date </div> <div class="ride-info-content" > 17.03.2017 </div> </div> <div class="drive-row"> <div class="ride-infotitle"> Driving Score </div> <div class="ride-info-content" style="color: #27ae60"> Very Safe Drive </div> </div> <!-- <div class="drive-row"> <div class="route-from-section"> <div class="ride-infotitle"> Distance </div> <div class="ride-info-content"> 233 km </div> </div> <div class="route-to-section"> <div class="ride-infotitle"> Time </div> <div class="ride-info-content"> 2h 33min </div> </div> <br style="clear: left;" /> </div> --> <div class="drive-row"> <div class="ride-infotitle"> Insurance Cost </div> <div class="ride-info-content"> CHF 9.15 </div> </div> <div class="drive-moreinfo-wrapper"> <button type="button" class="btn btn-primary btn-md drive-details-btn"> Details </button> </div> </div> </div> </div>'
+            $('.journies').append(drive);
         }
 
-        $('.journeys').append('drive');
+        
     }
 
     function loadCarStatus(car) {
@@ -20,11 +21,10 @@ $( document ).ready(function() {
     }
 
     //Get Car info
-    $.get( "/car/1", function( data ) {
+    $.get( API + "/car/1", function( data ) {
       
       car = data;
-
-      loadDrives(car.drives);
+      loadDrives(car.Drives);
 
       loadCarStatus(car);
 
@@ -32,3 +32,18 @@ $( document ).ready(function() {
 
 
 });
+
+// Navigation
+$( document ).ready(function() {
+    // Hide all section
+    $("section").hide();
+
+    $("section#intro").show();
+
+    $("a.page-scroll").on("click", function(){
+        $("section").hide();
+        $("section#"+ $(this).data('section')).show();
+    });
+
+});
+    
--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -12,8 +12,9 @@ $(function() {
     $(document).on('click', 'a.page-scroll', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
-            scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+            scrollTop: 0
+            //scrollTop: $($anchor.attr('href')).offset().top
+        }, 1000, 'easeInOutExpo');
         event.preventDefault();
     });
 });
